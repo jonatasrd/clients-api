c8efe537ef3fdbc526258bd98157c8adac78b27d
Jonatas Ramos Domingos <jonatas.domingos@dasa.com.br>
2021-03-05 17:46:04 -0300

    fixed usecase

--- src/main/kotlin/br/com/luizalabs/clientsapi/usecase/AddToWishListUseCase.kt
+++ src/main/kotlin/br/com/luizalabs/clientsapi/usecase/AddToWishListUseCase.kt
@@ -1,35 +1,28 @@
 package br.com.luizalabs.clientsapi.usecase
 
-import br.com.luizalabs.clientsapi.domain.exception.ResourceNotFoundException
 import br.com.luizalabs.clientsapi.repository.ClientMongoRepository
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class AddToWishListUseCase(
-    val findClientByIdUseCase: FindClientByIdUseCase,
     val getValidProductsIdsUseCase: GetValidProductsIdsUseCase,
     val clientMongoRepository: ClientMongoRepository
 ) {
 
-    private val logger: Logger = LoggerFactory.getLogger(javaClass)
-
     fun execute(clientId: String, productsIds: Set<String>) {
 
-        try {
-            val client = findClientByIdUseCase.execute(clientId)
+        val client = clientMongoRepository.findById(clientId)
+
+        if (client.isPresent) {
+
+            val clientMongoModel = client.get()
 
             val validProductsIds = getValidProductsIdsUseCase.execute(productsIds)
 
             if (validProductsIds.isNotEmpty()) {
-                client.wishlist.addAll(validProductsIds)
-
-                clientMongoRepository.save(client)
+                clientMongoModel.wishlist.addAll(validProductsIds)
+                clientMongoRepository.save(clientMongoModel)
             }
-
-        } catch (error: ResourceNotFoundException) {
-            logger.info(error.message)
         }
     }
 }

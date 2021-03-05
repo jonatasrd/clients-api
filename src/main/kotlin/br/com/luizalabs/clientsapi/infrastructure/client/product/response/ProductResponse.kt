f64bfd033498fdfab1da0b8b4de54d877ebdbec8
Jonatas Ramos Domingos <jonatas.domingos@dasa.com.br>
2021-03-05 10:40:40 -0300

    added response for product search

---
+++ src/main/kotlin/br/com/luizalabs/clientsapi/infrastructure/client/product/response/ProductResponse.kt
@@ -0,0 +1,10 @@
+package br.com.luizalabs.clientsapi.infrastructure.client.product.response
+
+data class ProductResponse(
+    val id: String,
+    val title: String,
+    val price: Double,
+    val brand: String,
+    val image: String,
+    val reviewScore: Double?,
+)

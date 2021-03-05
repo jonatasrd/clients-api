6d32902ba4c019781044f5c05baf1b6f3dcf50d4
Jonatas Ramos Domingos <jonatas.domingos@dasa.com.br>
2021-03-05 17:48:05 -0300

    changed to return a domain

--- src/main/kotlin/br/com/luizalabs/clientsapi/usecase/FindClientByIdUseCase.kt
+++ src/main/kotlin/br/com/luizalabs/clientsapi/usecase/FindClientByIdUseCase.kt
@@ -1,6 +1,7 @@
 package br.com.luizalabs.clientsapi.usecase
 
 import br.com.luizalabs.clientsapi.domain.exception.ResourceNotFoundException
+import br.com.luizalabs.clientsapi.domain.toDomain
 import br.com.luizalabs.clientsapi.repository.ClientMongoRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
@@ -10,6 +11,6 @@ class FindClientByIdUseCase(
     val clientMongoRepository: ClientMongoRepository
 ) {
 
-    fun execute(id: String) = clientMongoRepository.findByIdOrNull(id)
+    fun execute(id: String) = clientMongoRepository.findByIdOrNull(id)?.toDomain()
         ?: throw ResourceNotFoundException("Client not found by id: $id!")
 }

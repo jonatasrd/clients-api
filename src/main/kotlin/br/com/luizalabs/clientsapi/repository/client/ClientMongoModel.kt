15276fc214714f01c0537d692e8e9599238769a6
Jonatas Ramos Domingos <jonatas.domingos@dasa.com.br>
2021-03-04 22:22:13 -0300

    added mongo model

---
+++ src/main/kotlin/br/com/luizalabs/clientsapi/repository/client/ClientMongoModel.kt
@@ -0,0 +1,44 @@
+package br.com.luizalabs.clientsapi.repository.client
+
+import br.com.luizalabs.clientsapi.domain.EditClientMessage
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
+
+@Document(value = "client")
+data class ClientMongoModel(
+
+    @Id
+    val _id: String?,
+
+    @Field("code")
+    val name: String,
+
+    @Field("email")
+    @Indexed(unique = true)
+    val email: String,
+
+    @Field("wishlist")
+    val wishlist: Set<String> = emptySet()
+
+) {
+
+    /**
+     * Overloaded constructor without id.
+     */
+    constructor(
+        name: String,
+        email: String
+    ) : this(
+        _id = null,
+        name = name,
+        email = email
+    )
+}
+
+internal fun ClientMongoModel.toUpdate(editMessage: EditClientMessage) = ClientMongoModel(
+    _id = this._id,
+    name = editMessage.name ?: this.name,
+    email = editMessage.email ?: this.email
+)

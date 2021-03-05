e63497d627b4c29a966b3e4f8aca01cabbb23d40
Jonatas Ramos Domingos <jonatas.domingos@dasa.com.br>
2021-03-04 22:22:01 -0300

    added new response

---
+++ src/main/kotlin/br/com/luizalabs/clientsapi/infrastructure/http/presentation/response/ClientCreateResponse.kt
@@ -0,0 +1,23 @@
+package br.com.luizalabs.clientsapi.infrastructure.http.presentation.response
+
+import br.com.luizalabs.clientsapi.domain.Client
+import io.swagger.annotations.ApiModel
+
+/**
+ * Client payload response.
+ */
+@ApiModel(value = "ClientResponse")
+data class ClientCreateResponse(
+    val id: String,
+    val name: String,
+    val email: String
+)
+
+/**
+ * Convert [Client] to [ClientCreateResponse].
+ */
+internal fun Client.toCreateResponse() = ClientCreateResponse(
+    id = this.id!!,
+    name = this.name,
+    email = this.email
+)

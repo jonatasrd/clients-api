1db15f9edb64c90885e503611230fd5ee43f9ba8
Jonatas Ramos <jonatas.domingos@dasa.com.br>
2021-03-05 22:15:51 -0300

    added fixture

--- src/test/kotlin/br/com/luizalabs/fixtures/consts/ClientConsFixture.kt
+++ src/test/kotlin/br/com/luizalabs/fixtures/consts/ClientConsFixture.kt
@@ -5,12 +5,17 @@ open class ClientConsFixture {
     companion object {
         const val DEFAULT_ID = "7be7243b-e938-fe5d-a71e-905fd7a9fbba"
         const val OTHER_ID = "7be7243b-e938-fe5d-a71e-905fd7a9fbbb"
+
         const val DEFAULT_NAME = "JONATAS RAMOS"
         const val OTHER_NAME = "JAQUELINE ALENCAR"
+
         const val DEFAULT_EMAIL = "jonatasrd@gmail.com"
         const val OTHER_EMAIL = "jaqueline@gmail.com"
         const val INVALID_EMAIL = "invalid"
+
         const val DEFAULT_ID_PRODUCT = "571fa8cc-2ee7-5ab4-b388-06d55fd8ab2f"
+        const val OTHER_ID_PRODUCT = "571fa8cc-2ee7-5ab4-b388-06d55fd8ab2ss"
+        const val ANY_ID_PRODUCT = "571fa8cc-2ee7-5ab4-b388-06d55fd8ab2dd"
 
     }
 }
